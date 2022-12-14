1ce4166bb0591345d9eb6b699cd2e20c0c87e1f8
eduardo diaz <eduardodiaz06@gmail.com>
2022-12-13 21:31:43 -0300

    segunda entrega js

---
+++ index.js
@@ -0,0 +1,39 @@
+//ordenar productos por nombre (sort)
+const productosOrdenados = [...productos]
+productosOrdenados.sort((a, b) => {
+
+    const nombreA = a.nombre;
+    const nombreB = b.nombre;
+    if (nombreA < nombreB) {
+        return -1;
+    }
+    if (nombreA > nombreB) {
+        return 1;
+    }
+    return 0;
+});
+
+//ver los productos en oferta en tabla (filter)
+const productosConOferta = productosOrdenados.filter(function (element) {
+    return element.oferta === true;
+})
+if (confirm("Quieres ver produtos en oferta?") === true) {
+
+    console.table(productosConOferta)
+}
+else console.table(productosOrdenados)
+
+
+
+//buscar producto y mostrarlo en tabla (find)
+const buscador = prompt("Buscar producto")
+function buscarPorductos() {
+    const buscarProducto = productosOrdenados.find(function (element) {
+        return element.nombre == buscador;
+    })
+    if (buscarProducto) {
+        console.table(buscarProducto)
+    } else alert("producto no encontrado")
+}
+
+buscarPorductos(buscador)
---
+++ productos.js
@@ -0,0 +1,83 @@
+//creacion array con productos
+const productos = [
+{
+    "id":1,
+    "nombre":"taladro",
+    "precio":71400,
+    "descripcion":"TALADRO 13mm INALAMBRICO 18V 1BAT TP 813/18 C1 PRO",
+    "categoria":"Herramientas electricas",
+    "oferta":true
+},
+{
+    "id":2,
+    "nombre":"banquillo",
+    "precio":27000,
+    "descripcion":"BANQUILLOS MECANICOS 3 TONELADAS (PAR) 288-430mm T",
+    "categoria":"Taller automotriz",
+    "oferta":false
+},
+{
+    "id":3,
+    "nombre":"soldadora",
+    "precio":70000,
+    "descripcion":"SOLDADORA INV ARCO MANUAL 140 amp IE 6140/1/220",
+    "categoria":"Soldadoras",
+    "oferta":false
+},
+{
+    "id":4,
+    "nombre":"esmeril",
+    "precio":28000,
+    "descripcion":"ESMERIL 4 1/2'; 700W + 4 DISC VEL/VAR AA 415/2/220",
+    "categoria":"Herramientas electricas",
+    "oferta":false
+},
+{
+    "id":5,
+    "nombre":"gata",
+    "precio":130000,
+    "descripcion":"GATA CAIMAN 3 Toneladas T83001",
+    "categoria":"Taller automotriz",
+    "oferta":true
+},
+{
+    "id":6,
+    "nombre":"aspiradora",
+    "precio":69000,
+    "descripcion":"ASPIRADORA INALAMBRICA 18V 20L SIN BAT A818/0",
+    "categoria":"Lavado y aseo",
+    "oferta":false
+},
+{
+    "id":7,
+    "nombre":"tronzadora",
+    "precio":116000,
+    "descripcion":"TRONZADORA SENSITIVA 14'; 2500W + 3 discos CS814/4",
+    "categoria":"Maquinaria y equipos",
+    "oferta":true
+},
+{
+    "id":8,
+    "nombre":"motocompresor",
+    "precio":125000,
+    "descripcion":"MOTOCOMPRESOR 24 litros 2hp WD-2024",
+    "categoria":"Compresores",
+    "oferta":false
+},
+{
+    "id":9,
+    "nombre":"mesa",
+    "precio":139000,
+    "descripcion":"MESA de TRABAJO PLEGABLE TSB4700",
+    "categoria":"Taller automotriz",
+    "oferta":true
+},
+{
+    "id":10,
+    "nombre":"transpaleta",
+    "precio":680000,
+    "descripcion":"TRANSPALETA TIJERA 1000kg Alt 800mm 540x1170mm",
+    "categoria":"Maquinaria y equipo",
+    "oferta":false
+}
+]
